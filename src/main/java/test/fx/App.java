a3de2eec96ab5c2d68a55243ebb1ceefe4de89ba
leshey <leshey@SENIOR>
2017-06-07 11:47:43 +0300

    It's a first code

---
+++ src/main/java/test/fx/App.java
@@ -0,0 +1,28 @@
+package test.fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        Scene scene = new Scene(root, 300, 275);
+        primaryStage.setTitle("Todolist");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+ 	}
+}
