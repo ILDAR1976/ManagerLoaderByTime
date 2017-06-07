ae6cfd6a936a447f79e339ea6740ab69bda59a62
leshey <leshey@SENIOR>
2017-06-07 12:50:33 +0300

    Add MVC

--- src/main/java/test/fx/App.java
+++ src/main/java/test/fx/App.java
@@ -1,28 +1,86 @@
 package test.fx;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
- * Hello world!
+ *  Main module
  *
  */
+
 public class App extends Application
 {
-    public static void main( String[] args )
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+	public static void main( String[] args )
     {
         launch(args);
     }
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-        Scene scene = new Scene(root, 300, 275);
-        primaryStage.setTitle("Todolist");
-        primaryStage.setScene(scene);
-        primaryStage.show();
- 	}
+	    this.primaryStage = primaryStage;
+	    this.primaryStage.setTitle("Loader manager");
+
+	    initRootLayout();
+	    showPersonOverview();
+	}
+
+	/**
+     * Инициализирует корневой макет.
+     */
+    public void initRootLayout() {
+        try {
+            // Загружаем корневой макет из fxml файла.
+            FXMLLoader loader = new FXMLLoader();
+            
+            loader.setLocation(App.class.getResource("view/RootLayout.fxml"));
+            
+            rootLayout = (BorderPane) loader.load();
+
+            // Отображаем сцену, содержащую корневой макет.
+            Scene scene = new Scene(rootLayout);
+            
+            primaryStage.setScene(scene);
+            
+            primaryStage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Показывает в корневом макете сведения об адресатах.
+     */
+    public void showPersonOverview() {
+        try {
+            // Загружаем сведения об адресатах.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Помещаем сведения об адресатах в центр корневого макета.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Возвращает главную сцену.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 }
+
